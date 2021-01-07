7ee6e969db7428c92b4eb18d94adb7db13fdeb3e
“erica_torrico” <ericatorrico@gmail.com>
2021-01-07 17:02:39 -0300

    First commit, including header

---
+++ js/components/banner.js
@@ -0,0 +1,12 @@
+Vue.component('banner', {
+    template: `
+    <header>
+        <div class="tag">
+            <p>Webinar</p>
+        </div>
+        <h1>El reto de humanizar el CX financiero en 2021.</h1>
+        <h2>La experiencia de un Unicornio de Latam</h2>
+        <p>Miércoles 16 de diciembre | 17 hs (Horario de Quito)</p>
+    </header>
+   `,
+});
